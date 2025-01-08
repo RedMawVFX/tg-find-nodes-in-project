06d8e9ed3b75b59b31be23fe3c81caf54d3845cc
Kevin Kipper <kevinkippervfx@socal.rr.com>
2025-01-08 11:23:26 -0800

    Add scripts, image, and documentation.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # tg-find-nodes-in-project
-Finds nodes in the active Terragen project.
+Finds nodes in the active Terragen project.  Once nodes have been found they can be added to or removed from the currently selected nodes in the project.
+
+The UI includes a search feature, the ability to resample the current contents in the project, and various ways to select nodes from the list.
+
+![tg_find_nodes_in_project GUI](/images/tg_find_nodes_in_project_gui.jpg)
+
+### Requirements
+Terragen 4 Professional v4.6.31 (or later) <br>
+or Terragen 4 Creative v4.7.15 (or later) <br>
+or Terragen 4 Free 4.7.15 (or later) <br>
+https://planetside.co.uk/
+
+terragen-rpc <br>
+https://github.com/planetside-software/terragen-rpc
+
+### Installation
+Install Terragen 4 on your computer. <br>
+Install the terragen_rpc module, via the pip install command. <br>
+Download this repository via “git clone [repository url]” <br>
+Terragen 4 should be running when you run this script. <br>
+
+In this repository you’ll find two Python scripts, which are identical except for their file extensions.  The file ending in .PY will open a command window when run, while the file ending in .PYW will not.  I recommend using the file with the .PYW extension when the script is run or called from an external file or controller device like a Tourbox.
+
+### Usage
+When the script is executed the UI will present a list of all the nodes at the root level of the project. You can refine the items displayed in the list by entering text in the “<b>Search pattern</b>” field, or click on the “<b>Clear</b>” button to clear the search pattern and display all the nodes.  The “<b>Refresh</b>” button updates the list with the current nodes in the project.
+
+Clicking the “<b>All</b>” button will select all the nodes displayed in the list, while clicking “<b>None</b>” will deselect all the nodes in the list.  Clicking “<b>Invert</b>” will toggle the selection state of the nodes in the list.
+
+Clicking the “<b>Add to selection</b>” button will add the selected nodes to the currently selected nodes in the project.  Clicking the “<b>Remove from selection</b>” button deselects the nodes from the currently selected nodes in the project.
+
+### Known Issues
+The script does not recursively look for nodes within the internal node network of another node.  It only retrieves nodes at the root level of the project.
+
+### Reference
+terragen-rpc <br>
+https://github.com/planetside-software/terragen-rpc
+
+Online documentation for Terragen RPC <br>
+https://planetside.co.uk/docs/terragen-rpc/
+
+Blog posts on using Terragen RPC <br>
+https://planetside.co.uk/blog/hello-rpc-part-1-scripting-for-terragen/ <br>
+https://planetside.co.uk/blog/hello-rpc-part-2-error-handling/ <br>
+https://planetside.co.uk/blog/script-it-yourself-kelvin-sunlight-colour-with-terragen-rpc/
---
+++ tg_find_nodes_in_project.py
@@ -0,0 +1,347 @@
+'''
+tg_find_nodes_in_project.py - Finds nodes in the Terragen project.  Can add or
+remove them from the currently selected nodes in the scene.
+'''
+import os.path
+import traceback
+import tkinter as tk
+from tkinter import messagebox
+import terragen_rpc as tg
+
+gui = tk.Tk()
+gui.geometry("250x460")
+gui.title(os.path.basename(__file__))
+gui_colours = ["#6f8e90", "#9db2b3", "#DBD0BD", "#D8C3BD", "#C1BDDB", "#DBBDC4"]
+gui.configure(bg=gui_colours[0])
+
+frame0 = tk.Frame(gui, relief=tk.FLAT, bg=gui_colours[1])
+frame1 = tk.Frame(gui, relief=tk.FLAT, bg=gui_colours[1])
+frame2 = tk.LabelFrame(gui, relief=tk.FLAT, text="Quick select in listbox ", bg=gui_colours[1])
+frame3 = tk.LabelFrame(
+    gui,
+    relief=tk.FLAT,
+    text="Select / deselect nodes in Terragen ",
+    bg=gui_colours[1]
+    )
+frame0.grid(row=0, column=0, padx=6, pady=6, sticky="WENS")
+frame1.grid(row=1, column=0, padx=6, pady=6, sticky="WENS")
+frame2.grid(row=2, column=0, padx=6, pady=6, sticky="WENS")
+frame3.grid(row=3, column=0, padx=6, pady=6, sticky="WENS")
+
+COLOUR_SEARCH_WITH_VALUE = "#E8D7F2" # light purple
+COLOUR_SEARCH_WITHOUT_VALUE = ""
+
+def popup_warning(title, message) -> None:
+    '''
+    Opens a window and displays a message.
+
+    Args:
+        title (str): Characters displayed at the top of the window.
+        message (str): Characters displayed in the body of the window.
+
+    Returns:
+        None
+    '''
+    messagebox.showwarning(title, message)
+
+def update_listbox(event=None):
+    '''
+    Removes items in the listbox that don't match search pattern.
+    Can be called by an event.
+
+    Returns:
+        None
+    '''
+    search_term = search_var.get().lower()
+    if len(search_term) > 0:
+        item_colour = COLOUR_SEARCH_WITH_VALUE
+    else:
+        item_colour = COLOUR_SEARCH_WITHOUT_VALUE
+    listbox.delete(0, tk.END)
+    for i in child_paths:
+        if search_term in i.lower():
+            listbox.insert(tk.END, i)
+            listbox.itemconfig(tk.END, {'bg': item_colour})
+
+def on_add_to_project() -> None:
+    '''
+    Adds selected items in the listbox to the selected nodes in the project.
+
+    Returns:
+        None
+    '''
+    if child_paths:
+        listbox_selection = listbox.curselection()
+        if listbox_selection:
+            for index in listbox_selection:
+                selected_path = listbox.get(index)
+                node_id = get_node_id_from_path(selected_path)
+                if node_id:
+                    select_deselect_in_project(node_id=node_id, mode="add")
+
+def on_remove_from_project() -> None:
+    '''
+    Removes nodes selected in the listbox from the current selection of nodes
+    in the project.
+
+    Returns: 
+        None
+    '''
+    listbox_selection = []
+    if child_paths:
+        listbox_selection = listbox.curselection()
+        if listbox_selection:
+            for index in listbox_selection:
+                selected_path = listbox.get(index)
+                node_id = get_node_id_from_path(selected_path)
+                if node_id:
+                    selected_nodes_in_project = select_deselect_in_project(
+                        node_id=None,
+                        mode="current"
+                        )
+                    if selected_nodes_in_project:
+                        currently_selected_node_paths = []
+                        for selected_node in selected_nodes_in_project:
+                            selected_node_path = get_path(selected_node)
+                            currently_selected_node_paths.append(selected_node_path)
+                        if selected_path in currently_selected_node_paths:
+                            currently_selected_node_paths.remove(selected_path)
+                        select_deselect_in_project(node_id=None, mode="clear")
+                        node_ids_to_select = []
+                        for path in currently_selected_node_paths:
+                            new_node_id = get_node_id_from_path(path)
+                            node_ids_to_select.append(new_node_id)
+                        for selected_id in node_ids_to_select:
+                            select_deselect_in_project(selected_id, mode="add")
+
+def select_deselect_in_project(node_id=None, mode=None):
+    '''
+    This function unifies several rpc method calls under one exception
+    handler for better readability.
+
+    Args:
+        node_id (obj): Node id
+        mode (str): Action to perform: current, add, clear
+
+    Return:
+        current_selection (list): Node ids or None
+    '''
+    try:
+        match mode:
+            case "current":
+                return tg.current_selection()
+            case "add":
+                tg.select_more(node_id)
+                return None
+            case "clear":
+                tg.select_none()
+                return None
+    except ConnectionError as e:
+        popup_warning("Terragen RPC connection error", str(e))
+    except TimeoutError as e:
+        popup_warning("Terragen RPC timeout error", str(e))
+    except tg.ReplyError as e:
+        popup_warning("Terragen RPC reply error", str(e))
+    except tg.ApiError:
+        popup_warning("Terragen RPC API error", traceback.format_exc())
+
+def get_node_id_from_path(path):
+    '''
+    Gets the node id from the given path.
+
+    Returns
+        <obj>: A node id or None. 
+    '''
+    try:
+        return tg.node_by_path(path)
+    except ConnectionError as e:
+        popup_warning("Terragen RPC connection error", str(e))
+    except TimeoutError as e:
+        popup_warning("Terragen RPC timeout error", str(e))
+    except tg.ReplyError as e:
+        popup_warning("Terragen RPC reply error", str(e))
+    except tg.ApiError:
+        popup_warning("Terragen RPC API error", traceback.format_exc())
+
+def listbox_select_all() -> None:
+    '''
+    Selects all items in the listbox.
+
+    Returns:
+        None
+    '''
+    listbox.selection_set(0,tk.END)
+
+def listbox_select_none() -> None:
+    '''
+    Deselects all items in the listbox.
+
+    Returns:
+        None
+    '''
+    listbox.selection_clear(0,tk.END)
+
+def listbox_select_invert() -> None:
+    '''
+    Inverts listbox selection.
+
+    Returns:
+        None
+    '''
+    total_items = listbox.size()
+    for i in range(total_items):
+        if i not in listbox.curselection():
+            listbox.select_set(i)
+        else:
+            listbox.select_clear(i)
+
+def on_refresh() -> None:
+    '''
+    Triggers function calls to resample project file for latest nodes.
+    Refreshes the contents of the listbox.
+
+    Returns:
+        None
+    '''
+    global child_paths
+    child_paths = get_root_level_nodes()
+    listbox.delete(0,tk.END)
+    for path in child_paths:
+        listbox.insert(tk.END, path)
+    update_listbox()
+
+def get_paths_of_child_nodes(child_node_ids_filtered):
+    '''    
+    Gets path of nodes and sorts in alphanumeric order.
+
+    Args:
+        child_node_ids_filtered (list): Object ids of root level child nodes.
+
+    Returns:
+        paths (list): Node paths in alphanumeric order.
+    '''
+    paths = []
+    if child_node_ids_filtered:
+        for child in child_node_ids_filtered:
+            child_path = get_path(child)
+            if child_path:
+                paths.append(child_path)
+    if paths:
+        paths.sort()
+    return paths
+
+def get_path(child_obj):
+    '''
+    Gets the path of a node.
+
+    Args:
+        child_obj (obj): Child id as object (memory address)
+
+    Returns:
+        child_path (str): Path of child node, i.e. /Sunlight 01
+    '''
+    try:
+        child_path = child_obj.path()
+        return child_path
+    except ConnectionError as e:
+        popup_warning("Terragen RPC connection error", str(e))
+    except TimeoutError as e:
+        popup_warning("Terragen RPC timeout error", str(e))
+    except tg.ReplyError as e:
+        popup_warning("Terragen RPC reply error", str(e))
+    except tg.ApiError:
+        popup_warning("Terragen RPC API error", traceback.format_exc())
+
+def get_root_level_nodes():
+    '''
+    Gets all root level nodes in the project and calls function to determine paths.
+
+    Returns:
+        child_paths (list): Child node paths sorted alphnumerically.    
+    '''
+    try:
+        project = tg.root()
+        child_node_ids = project.children()
+        group_node_ids = project.children_filtered_by_class("group")
+        child_node_ids_filtered = remove_group_nodes(child_node_ids, group_node_ids)
+        child_paths_sorted = get_paths_of_child_nodes(child_node_ids_filtered)
+        return child_paths_sorted
+    except ConnectionError as e:
+        popup_warning("Terragen RPC connection error", str(e))
+    except TimeoutError as e:
+        popup_warning("Terragen RPC timeout error", str(e))
+    except tg.ReplyError as e:
+        popup_warning("Terragen RPC reply error", str(e))
+    except tg.ApiError:
+        popup_warning("Terragen RPC API error", traceback.format_exc())
+
+def remove_group_nodes(child_node_ids, group_node_ids):
+    '''
+    Removes group nodes from list of child nodes, because object instances
+    are not hashable and can't use set commands.
+
+    Args:
+        child_node_ids (list): Object ids of root level child nodes.
+        group_node_ids (list): Object ids of group nodes.
+
+    Returns:
+        child_node_ids_filtered (list): Object ids of child nodes
+    '''
+    child_node_ids_filtered = []
+    if child_node_ids:
+        for child in child_node_ids:
+            if child not in group_node_ids:
+                child_node_ids_filtered.append(child)
+    return child_node_ids_filtered
+
+def on_clear() -> None:
+    '''
+    Clears search variable and updates the listbox.
+
+    Returns:
+        None
+    '''
+    search_var.set("")
+    update_listbox()
+
+# main
+child_paths = get_root_level_nodes()
+
+# tkinter variables
+search_var = tk.StringVar()
+
+# gui frame 0
+tk.Label(frame0, text="Search pattern: ", bg=gui_colours[1]).grid(row=0, column=0, pady=4)
+search_e = tk.Entry(frame0, textvariable=search_var, bg=COLOUR_SEARCH_WITH_VALUE)
+search_e.grid(row=0, column=1)
+search_e.bind("<KeyRelease>", update_listbox)
+
+clear_b = tk.Button(frame0, text="Clear", command=on_clear, width=6)
+clear_b.grid(row=1, column=0, padx=4, pady=4, sticky="w")
+
+# gui frame1
+listbox = tk.Listbox(frame1, width=36, selectmode=tk.MULTIPLE)
+listbox.grid(row=0, column=0, columnspan=3, sticky="nsew")
+for item in child_paths:
+    listbox.insert(tk.END, item)
+scrollbar = tk.Scrollbar(frame1, orient=tk.VERTICAL, command=listbox.yview)
+scrollbar.grid(row=0, column=3, sticky="ns", columnspan=4)
+listbox.config(yscrollcommand=scrollbar.set)
+
+refresh_b = tk.Button(frame1, text="Refresh", command=on_refresh, width=6)
+refresh_b.grid(row=1, column=0, padx=4, pady=6, sticky="w")
+
+listbox_select_all_b = tk.Button(frame2, text="All", command=listbox_select_all, width=6)
+listbox_select_all_b.grid(row=0, column=0, padx=4, pady=6, sticky="w")
+listbox_select_none_b = tk.Button(frame2, text="None", command=listbox_select_none, width=6)
+listbox_select_none_b.grid(row=0, column=1, padx=4, pady=6, sticky="w")
+listbox_select_invert_b = tk.Button(frame2, text="Invert", command=listbox_select_invert, width=6)
+listbox_select_invert_b.grid(row=0, column=2, padx=4, pady=6, sticky="w")
+
+# gui frame3
+all_b = tk.Button(frame3, text="Add to selection", command=on_add_to_project, width=18)
+all_b.grid(row=0, column=2, padx=4, pady=4, sticky="w")
+none_b = tk.Button(frame3, text="Remove from selection", command=on_remove_from_project, width=18)
+none_b.grid(row=1, column=2, padx=4, pady=4, sticky="w")
+
+gui.mainloop()
